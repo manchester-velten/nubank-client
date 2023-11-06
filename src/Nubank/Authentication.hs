bafd41364d6b1334b6f26d89667338eccf6388a1
Ismael Velten <ismael.velten@paybyrd.com>
2023-11-06 16:13:22 -0300

    Creating auto refresh routine for authentication

--- src/Nubank/Authentication.hs
+++ src/Nubank/Authentication.hs
@@ -1,18 +1,16 @@
 module Nubank.Authentication
-  ( Credentials (..)
+  ( AuthenticationMethod (..)
   , Authentication (..)
   , AuthenticationState (..)
-  , getAuthenticationFromResponse
   , Authenticated (..)
+  , authenticate
   ) where
 
-import Nubank.Login
 import Data.Time
+import Nubank.Login
 
-data Credentials = PasswordAuthentication
-  { login    :: String
-  , password :: Password
-  } deriving (Show, Eq)
+data AuthenticationMethod = LoginAndPassword Login Password
+  deriving (Show, Eq)
 
 data Authentication = Authentication
   { token      :: String
@@ -21,37 +19,61 @@ data Authentication = Authentication
   } deriving (Show, Eq)
 
 data AuthenticationState = AuthenticationState
-  { credentials    :: Credentials
-  , authentication :: Authentication
+  { authenticationMethod :: AuthenticationMethod
+  , authentication       :: Authentication
   } deriving (Show, Eq)
 
-getAuthenticationFromResponse :: PasswordAuthenticationResponse -> Authentication
-getAuthenticationFromResponse response =
-  Authentication
-    { token      = accessToken response
-    , links      = _links response
-    , expiration = refreshBefore response
-    }
+applyAuth :: Authenticated a -> AuthenticationState -> IO (a, AuthenticationState)
+applyAuth (Authenticated op) = op
 
-authenticatedOperation :: Authenticated a -> AuthenticationState -> IO (a, AuthenticationState)
-authenticatedOperation (Authenticated op) = op
+getAuthenticationState :: AuthenticationMethod -> IO AuthenticationState
+getAuthenticationState (LoginAndPassword l p) = do
+  authResponse <- passwordAuthSimple l p
+  let auth = Authentication
+        { token      = accessToken authResponse
+        , links      = _links authResponse
+        , expiration = refreshBefore authResponse
+        }
+  let state = AuthenticationState
+        { authenticationMethod = LoginAndPassword l p
+        , authentication       = auth }
+  return state
+
+autoRenewAuthentication :: AuthenticationState -> IO AuthenticationState
+autoRenewAuthentication s = do
+  currentTime <- getCurrentTime
+  let expirationTime = expiration (authentication s)
+  let diff           = nominalDiffTimeToSeconds $ diffUTCTime expirationTime currentTime
+  let method         = authenticationMethod s
+  if diff <= 10
+    then getAuthenticationState method
+    else return s
 
 newtype Authenticated a = Authenticated (AuthenticationState -> IO (a, AuthenticationState))
 
 instance Functor Authenticated where
   fmap f op = Authenticated (\s -> do
-    (x, s') <- authenticatedOperation op s
-    return (f x, s'))
+    s' <- autoRenewAuthentication s
+    (x, s'') <- applyAuth op s'
+    return (f x, s''))
 
 instance Applicative Authenticated where
-  pure x = Authenticated (\s ->
-    do return (x, s))
+  pure x = Authenticated (\s -> do
+    s' <- autoRenewAuthentication s
+    return (x, s'))
   xf <*> xa = Authenticated (\s -> do
-    (f, s')  <- authenticatedOperation xf s
-    (x, s'') <- authenticatedOperation xa s'
-    return (f x, s''))
+    s' <- autoRenewAuthentication s
+    (f, s'')  <- applyAuth xf s'
+    (x, s''') <- applyAuth xa s''
+    return (f x, s'''))
 
 instance Monad Authenticated where
   x >>= f = Authenticated (\s -> do
-    (x', s') <- authenticatedOperation x s
-    authenticatedOperation (f x') s')
+    s' <- autoRenewAuthentication s
+    (x', s'') <- applyAuth x s'
+    applyAuth (f x') s'')
+
+authenticate :: AuthenticationMethod -> Authenticated AuthenticationState
+authenticate method = Authenticated (\s -> do
+  state <- getAuthenticationState method
+  return (s, state))
